03deeb7f87c5c0d73c1e4849d55ce3bf39c7937b
Leora <cirem576@gmail.com>
2024-02-01 01:21:45 +0300

    splashscreen basics

--- GithubUsersMobile/App.js
+++ GithubUsersMobile/App.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { Text, View, StyleSheet, Image, TextInput, FlatList, TouchableOpacity } from 'react-native';
 import axios from 'axios';
+import SearchResult from './SearchResult';
 
 export default function App() {
   const [appIsReady, setAppIsReady] = useState(false);
   const [searchText, setSearchText] = useState('');
   const [searchResults, setSearchResults] = useState([]);
-
+  const [totalResults, setTotalResults] = useState(0);
   useEffect(() => {
     prepareApp();
   }, []);
@@ -19,24 +20,24 @@ export default function App() {
       console.error('Uygulama hazırlanırken bir hata oluştu:', error);
     }
   };
-let timeout = null
+
+  let timeout = null;
   const searchUser = async (text) => {
     if (timeout) {
-      clearTimeout(timeout)
-      timeout = null
+      clearTimeout(timeout);
+      timeout = null;
     }
     try {
       timeout = setTimeout(async () => {
         const response = await axios.get(`https://api.github.com/search/users?q=${text}`);
-      console.log('API Cevabı:', response.data); 
-      setSearchResults(response.data.items);
+        console.log('API Cevabı:', response.data);
+        setSearchResults(response.data.items);
+        setTotalResults(response.data.total_count);
       }, 1000);
-      
     } catch (error) {
       console.error('Kullanıcı arama hatası:', error);
     }
   };
-  
 
   if (!appIsReady) {
     return (
@@ -65,12 +66,28 @@ let timeout = null
         </View>
       </View>
       <View style={[styles.grayBackground, { flex: 7 }]}>
+        <Text style={styles.totalResultsText}>
+          <Text>Total </Text>
+          <Text style={styles.redText}>{totalResults}</Text>
+          <Text> results found.</Text>
+        </Text>
         <FlatList
+          key={totalResults}
           data={searchResults}
           keyExtractor={(item) => item.id.toString()}
+          numColumns={3}
           renderItem={({ item }) => (
             <TouchableOpacity style={styles.userItem}>
-              <Text>{item.login}</Text>
+              <View style={[styles.userItem, { marginBottom: 10 }]}>
+                <View style={styles.avatarContainer}>
+                  <View style={styles.avatarBackground}></View>
+                  <Image source={{ uri: item.avatar_url }} style={styles.avatar} />
+                </View>
+                <View style={styles.userInfo}>
+                  <Text style={styles.username}>@{item.login}</Text>
+                </View>
+              </View>
+
             </TouchableOpacity>
           )}
         />
@@ -99,7 +116,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     marginTop: 20,
-    paddingHorizontal: 10, 
+    paddingHorizontal: 10,
   },
   searchInput: {
     flex: 1,
@@ -108,13 +125,69 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   searchIcon: {
-    width: 10,
-    height: 10,
-    marginRight: 5, 
+    width: '0%',
+    height: '0%',
+    marginRight: 3,
+    justifyContent: 'center',
   },
   userItem: {
     padding: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: '#CCCCCC',
+    width: '33%',
+    aspectRatio: 1,
+    alignItems: 'center',
+  },
+  userContainer: {
+    alignItems: 'center',
+  },
+  avatarContainer: {
+    width: 90,
+    height: 90,
+    borderRadius: 10,
+    marginBottom: 10,
+    position: 'relative',
+    overflow: 'visible',
+  },
+  avatarBackground: {
+    position: 'absolute',
+    backgroundColor: '#1D1D1D',
+    width: '100%',
+    height: '100%',
+    borderRadius: 10,
+
+  },
+  avatar: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    position: 'absolute',
+    top: -15,
+    left: '50%',
+    transform: [{ translateX: -40 }],
+    borderWidth: 5,
+    borderColor: '#1D1D1D',
+  },
+  userInfo: {
+    position: 'absolute',
+    top: '550%',
+    width: 100,
+    flex: 1,
+    textAlign: 'center',
+  },
+
+  username: {
+    fontWeight: 'bold',
+    marginTop: 5,
+    color: '#ffff',
+    fontSize: 10,
+    width: 100,
+    textAlign: 'center',
+  },
+
+  totalResultsText: {
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  redText: {
+    color: 'red',
   },
 });
--- GithubUsersMobile/SearchResult.js
+++ GithubUsersMobile/SearchResult.js
@@ -2,11 +2,10 @@ import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 
 const SearchResult = ({ user }) => {
-
   return (
     <View style={styles.container}>
       <View style={styles.userContainer}>
-        <Image source={{ uri: avatar_url }} style={styles.avatar} />
+        <Image source={{ uri: user.avatar_url }} style={styles.avatar} />
         <Text style={styles.username}>{user.login}</Text>
       </View>
     </View>
@@ -16,9 +15,9 @@ const SearchResult = ({ user }) => {
 const styles = StyleSheet.create({
   container: {
     marginVertical: 5,
-    backgroundColor: 'black', // Siyah arka plan
-    borderRadius: 10, // Oval şeklinde yapmak için
-    overflow: 'hidden', // Oval şekli sağlamak için
+    backgroundColor: 'black',
+    borderRadius: 10,
+    overflow: 'hidden',
   },
   userContainer: {
     flexDirection: 'row',
@@ -34,7 +33,7 @@ const styles = StyleSheet.create({
   username: {
     fontWeight: 'bold',
     fontSize: 16,
-    color: 'white', // Kullanıcı adının rengi
+    color: 'white',
   },
 });
 
